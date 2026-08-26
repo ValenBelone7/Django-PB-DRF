ef4b4ec0de2c5354cf0e8b6db6cf83bf1a72d1bf
Valen Belone <valenbelone14@gmail.com>
2026-08-25 22:02:27 -0300

    Implementa CRUD de herederos

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from .views import heredero_detail, herederos
 
-urlpatterns = []
+urlpatterns = [
+	path('herederos/', herederos, name='herederos_api'),
+	path('herederos/<int:pk>/', heredero_detail, name='heredero_detail_api'),
+]
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Heredero
+from .serializers import HerederoSerializer
+
+
+@api_view(['GET', 'POST'])
+def herederos(request):
+	if request.method == 'GET':
+		queryset = Heredero.objects.all()
+		serializer = HerederoSerializer(queryset, many=True)
+		return Response(serializer.data, status=status.HTTP_200_OK)
+
+	serializer = HerederoSerializer(data=request.data)
+	if serializer.is_valid():
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
+	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def heredero_detail(request, pk):
+	heredero = get_object_or_404(Heredero, pk=pk)
+
+	if request.method == 'GET':
+		serializer = HerederoSerializer(heredero)
+		return Response(serializer.data, status=status.HTTP_200_OK)
+
+	if request.method == 'PUT':
+		serializer = HerederoSerializer(heredero, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_200_OK)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	heredero.delete()
+	return Response(status=status.HTTP_204_NO_CONTENT)
